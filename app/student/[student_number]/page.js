757e10a179c1a14a7f40eefdccf7239b85c1c40f
Elif <elif.keskin233@gmail.com>
2024-09-27 17:51:54 +0300

    class informations added to details page

--- app/student/[student_number]/page.js
+++ app/student/[student_number]/page.js
@@ -8,6 +8,11 @@ const genderSymbols = {
   female: '♀',
 };
 
+const classInfo = {
+  class_name: '9. Dönem Frontend Uzmanlık Sınıfı - Furkan Özay',
+  period: '9',
+  location: 'Class 117',
+};
 
 const StudentProfile = async ({ params }) => {
   const students = await getStudentsData();
@@ -30,42 +35,52 @@ const StudentProfile = async ({ params }) => {
 
         </h1>
       </div>
-      <ul className="student-info">
-        <li>
-          <span className="label">Student Number:</span>
-          <span className="value">{student.student_number}</span>
-        </li>
-        <li>
-          <span className="label">Gender:</span>
+      <div className="info-container">
+  <ul className="student-info">
+    <li>
+      <span className="label">Student Number:</span>
+      <span className="value">{student.student_number}</span>
+    </li>
+    <li>
+      <span className="label">Gender:</span>
+      <span
+        className={`gender-icon ${student.gender === 'male' ? 'blue' : 'pink'}`}
+      >
+        {genderSymbols[student.gender]}
+      </span>
+    </li>
+    <li>
+      <span className="label">Absences:</span>
+      <span className="value">
+        <span className={`status ${student.absences > 5 ? 'red' : 'green'}`}>
+          {student.absences}
+        </span>
+      </span>
+    </li>
+    <li>
+      <span className="label">Assignment Average:</span>
+      <span className="value">{student.homework_average}</span>
+    </li>
+  </ul>
 
-          <span
-            className={`gender-icon ${student.gender === 'male' ? 'blue' : 'pink'
-              }`}
-          >
-            <span className="gender-icon">{genderSymbols[student.gender]}
-            </span>
+  <ul className="class-info">
+    <li>
+      <span className="label">Class Name:</span>
+      <span className="value">{classInfo.class_name}</span>
+    </li>
+    <li>
+      <span className="label">Period:</span>
+      <span className="value">{classInfo.period}</span>
+    </li>
+    <li>
+      <span className="label">Location:</span>
+      <span className="value">{classInfo.location}</span>
+    </li>
+  </ul>
+</div>
 
-          </span>
-
-        </li>
-        <li>
-          <span className="label">Absences:</span>
-          <span className="value">
-            {student.absences}{' '}
-            <span
-              className={`status ${student.absences > 5 ? 'red' : 'green'
-                }`}
-            >
-              {student.absences > 5 ? 'High' : 'Fine'}
-            </span>
-          </span>
-        </li>
-        <li>
-          <span className="label">Assignment Average:</span>
-          <span className="value">{student.homework_average}</span>
-        </li>
-      </ul>
-    </div>
+      </div>
+    
   );
 };
 
